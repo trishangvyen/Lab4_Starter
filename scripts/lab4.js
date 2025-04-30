24aab7c9ed49a5448eb3d70d8f9068e09468f6ba
trishangvyen <trn014@ucsd.edu>
2025-04-29 20:45:34 -0700

    Fixed buggy code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
@@ -25,8 +25,8 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
+    let discounted = []
+    let length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
         discountedPrice += prices[i] * (1 - discount);
